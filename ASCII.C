bcb50cb8c188676ff000411d4407dbe7d9409aeb
Rithiyga <rithiyga@gmail.com>
2024-09-08 23:13:07 +0530

    Add files via upload

---
+++ ASCII.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+clrscr();
+for(c=1;c<=37;c++)
+{
+for(d=1;d<=49;d++)
+{
+for(a=4;a<=4;a++)
+{
+for(b=3;b<=3;b++)
+printf("%c%c",a,b);
+}
+}
+}
+getch();
+}
